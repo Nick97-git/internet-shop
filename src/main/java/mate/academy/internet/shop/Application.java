c185b1871b3d9b0c065dc937e8a7d15583357a56
Nick97-git <nickarchangel97@gmail.com>
2020-04-22 18:59:31 +0300

    modified classes Storage and Application

--- src/main/java/mate/academy/internet/shop/Application.java
+++ src/main/java/mate/academy/internet/shop/Application.java
@@ -9,26 +9,19 @@ public class Application {
 
     public static void main(String[] args) {
         ItemService itemService = (ItemService) injector.getInstance(ItemService.class);
-        System.out.println("Create Items");
         itemService.create(new Item("item", 42.0));
         itemService.create(new Item("item1", 452.0));
         itemService.create(new Item("item2", 66.34));
         itemService.create(new Item("item3", 4.50));
 
-        System.out.println("All Items: ");
         itemService.getAll().forEach(it -> System.out.println(it.toString()));
 
-        System.out.println("Item by ID: ");
         Item item1 = itemService.get(2L);
         System.out.println(item1.toString());
 
-        System.out.println("Delete Item by ID.");
         itemService.delete(2L);
-
-        System.out.println("Update Item");
         itemService.update(itemService.get(3L));
 
-        System.out.println("Get All Items after delete and update: ");
         itemService.getAll().forEach(it -> System.out.println(it.toString()));
     }
 }
--- src/main/java/mate/academy/internet/shop/dao/Storage.java
+++ src/main/java/mate/academy/internet/shop/dao/Storage.java
@@ -2,10 +2,17 @@ package mate.academy.internet.shop.dao;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import mate.academy.internet.shop.model.Bucket;
 import mate.academy.internet.shop.model.Item;
+import mate.academy.internet.shop.model.Order;
+import mate.academy.internet.shop.model.User;
 
 public class Storage {
     public static List<Item> items = new ArrayList<>();
+    public static List<User> users = new ArrayList<>();
+    public static List<Order> orders = new ArrayList<>();
+    public static List<Bucket> buckets = new ArrayList<>();
     private static Long itemId = 0L;
     private static Long userId = 0L;
     private static Long bucketId = 0L;
@@ -14,5 +21,20 @@ public class Storage {
     public static void addItem(Item item) {
         item.setId(++itemId);
         items.add(item);
+    }
+
+    public static void addUser(User user) {
+        user.setId(++userId);
+        users.add(user);
+    }
+
+    public static void addOrder(Order order) {
+        order.setId(++orderId);
+        orders.add(order);
+    }
+
+    public static void addBucket(Bucket bucket) {
+        bucket.setId(++bucketId);
+        buckets.add(bucket);
     }
 }
