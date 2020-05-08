e03986d68b632caa632ed90f865d538e5b926470
Nick97-git <nickarchangel97@gmail.com>
2020-05-08 20:19:41 +0300

    modified DECIMAL in init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -3,7 +3,7 @@ CREATE SCHEMA `internet_shop` DEFAULT CHARACTER SET utf8;
 CREATE TABLE `internet_shop`.`products`(
     `product_id` BIGINT(11)   NOT NULL AUTO_INCREMENT,
     `name`       VARCHAR(255) NOT NULL,
-    `price`      DECIMAL(11)  NOT NULL,
+    `price`      DECIMAL(11,2)  NOT NULL,
     PRIMARY KEY (`product_id`)
 );
 
