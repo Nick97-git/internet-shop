e109dc2910e1453ab0bb0176c5a747ee1cc85ece
Nick97-git <nickarchangel97@gmail.com>
2020-05-08 20:12:53 +0300

    modified init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,8 +1,9 @@
-CREATE SCHEMA `internet_shop` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA `internet_shop` DEFAULT CHARACTER SET utf8;
 
-CREATE TABLE `internet_shop`.`products` (
-  `product_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  `price` DECIMAL(11) NOT NULL,
-  PRIMARY KEY (`product_id`),
-  UNIQUE INDEX `idproducts_UNIQUE` (`product_id` ASC) VISIBLE);
+CREATE TABLE `internet_shop`.`products`(
+    `product_id` BIGINT(11)   NOT NULL AUTO_INCREMENT,
+    `name`       VARCHAR(255) NOT NULL,
+    `price`      DECIMAL(11)  NOT NULL,
+    PRIMARY KEY (`product_id`)
+);
+
