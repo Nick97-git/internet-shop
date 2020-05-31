8a89f88b04c2eafa23e32745466c3313c89e11d6
Nick97-git <nickarchangel97@gmail.com>
2020-05-31 13:39:57 +0300

    added sonar

--- src/main/java/dev/internet/shop/util/HashUtil.java
+++ src/main/java/dev/internet/shop/util/HashUtil.java
@@ -7,7 +7,6 @@ import org.apache.log4j.Logger;
 
 public class HashUtil {
     private static final Logger LOGGER = Logger.getLogger(HashUtil.class);
-    private static final String HASH_FUNCTION = "SHA-512";
 
     public static byte[] getSalt() {
         SecureRandom random = new SecureRandom();
@@ -19,7 +18,7 @@ public class HashUtil {
     public static String hashPassword(String password, byte[] salt) {
         StringBuilder hashedPassword = new StringBuilder();
         try {
-            MessageDigest messageDigest = MessageDigest.getInstance(HASH_FUNCTION);
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
             messageDigest.update(salt);
             byte[] digest = messageDigest.digest(password.getBytes());
             for (byte b: digest) {
